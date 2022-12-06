668324e0fd8bb6a89180000a15af39c28791e0f6
pavan <107210843+pavanpradeep2@users.noreply.github.com>
2022-12-06 18:12:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,11 +25,11 @@ User = Client(
 
 @Bot.on_message(filters.private & filters.command("start"))
 async def start_handler(_, event: Message):
-	await event.reply_photo("https://telegra.ph/file/2b160d9765fe080c704d2.png",
+	await event.reply_photo("https://telegra.ph/HHii-11-21",
                                 caption=Config.START_MSG.format(event.from_user.mention),
                                 reply_markup=InlineKeyboardMarkup([
                                     [InlineKeyboardButton("🔺 Donate us 🔺", url="https://p.paytm.me/xCTH/vo37hii9")],
-                                    [InlineKeyboardButton("⚡️ LazyDeveloper ⚡️", url="https://t.me/LazyDeveloper")],
+                                    [InlineKeyboardButton("⚡️ LazyDeveloper ⚡️", url="https://t.me/roiderff")],
                                     [InlineKeyboardButton("🤒Help", callback_data="Help_msg"),
                                     InlineKeyboardButton("🦋About", callback_data="About_msg")]]))
 
@@ -38,8 +38,8 @@ async def help_handler(_, event: Message):
 
     await event.reply_text(Config.ABOUT_HELP_TEXT.format(event.from_user.mention),
         reply_markup=InlineKeyboardMarkup([
-            [InlineKeyboardButton("Updates", url="https://t.me/LazyDeveloper"),
-             InlineKeyboardButton("Support Group", url="https://t.me/LazyPrincessSupport"), 
+            [InlineKeyboardButton("Updates", url="https://t.me/roiderff"),
+             InlineKeyboardButton("Support Group", url="https://t.me/backup764"), 
              InlineKeyboardButton("About", callback_data="About_msg")]
         ])
     )
